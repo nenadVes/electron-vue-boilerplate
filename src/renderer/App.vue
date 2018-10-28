a1989714d0b16d3f62f70ed5fbbf100ff5e6b6a6
Ilija Nikolic <ilijanikolic@Ilijas-MacBook-Pro.local>
2018-10-28 13:44:06 +0100

    initial fe commit

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -4,6 +4,8 @@
   </div>
 </template>
 
+<script src="https://unpkg.com/vue/dist/vue.js"></script>
+<script src="https://unpkg.com/vue-i18n/dist/vue-i18n.js"></script>
 <script>
 export default {
   name: 'App'
